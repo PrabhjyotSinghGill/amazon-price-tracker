fb465b6becbbc222cb8c60ce6ec024b33179c97b
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-31 11:39:16 -0500

    simple_tracker.py

--- simple_tracker.py
+++ simple_tracker.py
@@ -1,9 +1,30 @@
+from .amazon_config import(
+    get_web_driver_options,
+    get_chrome_web_driver,
+    set_ignore_certificate_error,
+    set_browser_as_incognito,
+    set_automation_as_head_less,
+    NAME,
+    CURRENCY,
+    FILTERS,
+    BASE_URL,
+    DIRECTORY
+    )
+ 
 class GenerateReport:
     def __init__(self):
         pass
 
 class AmazonAPI:
     def __init__(self):
+        self.base_url = base_url
+        self.search_term = search_term 
+        options = get_web_driver_options()
+        set_ignore_certificate_error(options)
+        set_browser_as_incognito(options)
+        self.driver = get_chrome_web_driver(options)
+        self.currency = currency 
+        self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
         pass 
 
 if __name__ == '__main__':
